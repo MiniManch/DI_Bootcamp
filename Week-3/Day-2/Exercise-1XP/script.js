ca4603137cd434f843376d2017a5822918d68b83
MiniManch <Emaneulrokah96@gmail.com>
2023-01-16 22:37:57 +0200

    Adding siolution to Exercises XP Mandatory

--- Week-3/Day-2/Exercise-1XP/script.js
+++ Week-3/Day-2/Exercise-1XP/script.js
@@ -1,5 +1,6 @@
 // Exercise 1
-// 1	
+console.log("Exercise 1");
+// 1
 let favoriteFood = "Pizza";
 // 2
 
@@ -7,6 +8,7 @@ let favoriteMeal = "Breakfast";
 // 3
 
 // Exercise 2
+console.log("Exercise 2 Part 1");
 // Part1
 let printMeal = `I eat ${favoriteFood} at every ${favoriteMeal}`
 console.log(printMeal);
@@ -21,7 +23,9 @@ let myWatchedSeriesSentence = myWatchedSeries[0]+', ' + myWatchedSeries[1] + ', 
 // 3
 console.log('I watched 3 series : '+ myWatchedSeriesSentence);
 
-// Exercise 3
+// Exercise 2
+console.log("Exercise 2 part 2");
+
 // Part 2
 // 1
 myWatchedSeries[myWatchedSeries.indexOf("the big bang theory")] ='friends'
@@ -46,6 +50,8 @@ console.log('Printing 3rd letter of Money Heist',`${myWatchedSeries[2]}`.substr(
 console.log(myWatchedSeries);
 
 // Exercise 3
+console.log("Exercise 3");
+
 let tempCelsius = 30;
 let tempFarenheit = tempCelsius * 1.8 + 32;
 console.log('Printing temperatures.',`${tempCelsius}°C is ${tempFarenheit} `);
@@ -59,7 +65,135 @@ console.log('Printing temperatures.',`${tempCelsius}°C is ${tempFarenheit} `);
 
 // What is the value of c?
 // Prediction: undefined.
-let c;
-console.log(c);
+
+// Analyse the code below, what will be the outcome?
+// console.log(3 + 4 + '5');
+// Prediction: JS knows that we're trying to add numbers with strings so it will be 7+5 so 75. because 5 is a string.
+
+// Exercise 5
+console.log("Exercise 5");
+
+console.log(typeof(15));
+// Prediction: number
+// Actual: number
+
+console.log(typeof(5.5));
+// Prediction: number, i thought it might be like python or other languages where there's a difference between whole and decimal numbers, but I'm pretty confident.
+// Actual: number
+
+console.log(typeof(NaN));
+// Prediction: NaN? undefined maybe
+// Actual: number
+
+console.log(typeof("hello"));
+// Prediction: String
+// Actual: string
+
+console.log(typeof(true));
+// Prediction: Boolean, pretty self explanatory
+// Actual: boolean
+
+console.log(typeof(1 != 2));
+// Prediction:Boolean, i think the outcome will be false and that is a boolean value
+// Actual: boolean
+
+console.log("hamburger" + "s");
+// Prediction: "hamburgers"
+// Actual: "hamburgers"
+
+console.log("hamburgers" - "s");
+// Prediction: "hamburger"
+// Actual:NaN - I'm guessing you cant do any subtraction with strings :(
+
+console.log("1" + "3",typeof("1"+"3"));
+// Prediction: "13", because they're both strings its like combining both strings.
+// Actual: "13"
+
+console.log("1" - "3");
+// Prediction: undefined? I dont think there is any way to do that.
+// Actual:NaN
+
+console.log("johnny" + 5);
+// Prediction: perhaps "johnny5"? just guessing, either that or an error.
+// Actual:"johnny5"
+
+console.log("johnny" - 5);
+// Prediction: probably undefined or error.
+// Actual:NaN
+
+console.log(99 * "hello");
+// Prediction: probably 99times "hello" in a continuous string.
+// Actual:NaN
+
+console.log(1 != 1);
+// Prediction: boolean? probably false.
+// Actual:false
+
+console.log(1 == "1");
+// Prediction: True and boolean. because == checks if they have the same value not the same type.
+// Actual: true
+
+console.log(1 === "1");
+// Prediction:False. === checks for both value and same type.
+// Actual:false
+
+// Exercise 6
+console.log("Exercise 6");
+
+console.log(5 + "34");
+// Prediction: 534, number type - Because we're adding up a number and a string, it will add them together but weirdly.
+// Actual:534
+
+console.log(5 - "4");
+// Prediction: NaN, i think substracting a string will always result in NaN as seen in the previous exercise.
+// Actual:1 , i guess it can do that with 'number strings?'
+
+console.log(10 % 5);
+// Prediction:0, 10%5 means the remainder of dividing 10 by 5 which is 0.
+// Actual: 0
+
+console.log(5 % 10);
+// Prediction: 5, it isnt divisible in whole therefore its 5.
+// Actual: 5
+
+console.log("Java" + "Script");
+// Prediction:"JavaScript" its just adding up strings
+// Actual:"JavaScript"
+
+console.log(" " + " ");
+// Prediction:"  " its just adding up strings
+// Actual: "  "
+
+console.log(" " + 0,typeof(" "+ 0));
+// Prediction:" 0" or NaN.
+// Actual:" 0"
+
+console.log(true + true);
+// Prediction: 2 i think True =1 and False = 0 right? it is boolean after all.
+// Actual:2
+
+console.log(true + false);
+// Prediction: 1
+// Actual:1
+
+console.log(false + true);
+// Prediction: 1
+// Actual:1
+
+console.log(false - true);
+// Prediction: - 1
+// Actual:-1
+
+console.log(!true);
+// Prediction: false, not-true is False isnt it?
+// Actual:false
+
+console.log(3 - 4);
+// Prediction: -1 its just mathematics
+// Actual:-1
+
+console.log("Bob" - "bill");
+// Prediction:NaN, can't  subtract a string, it will be NaN.
+// Actual:NaN
 
 
