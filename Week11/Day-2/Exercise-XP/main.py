c175d3de522918c7ba47954a8fc027dc706d5a74
Emmanuel Rokah <36491225+MiniManch@users.noreply.github.com>
2023-03-13 18:25:18 +0200

    adding solution to exercise XP day 2

---
+++ Week11/Day-2/Exercise-XP/main.py
@@ -0,0 +1,26 @@
+# 🌟 Exercise 1
+# Instructions
+# Use pip to install the flask module.
+# Run the command import flask in a python shell.
+# Run the command dir(flask) to see the different methods that come along with flask.
+
+import flask
+print(dir(flask))
+
+# Using Flask, create a template for your CV.
+# Your CV should contain:
+# Your name
+# A picture
+# Your hobbies
+# Your skills
+# Your strengths
+# Your weaknesses
+# Bonus: Add some CSS
+# Hint : To add some CSS take a look at the video called Static Files in the Online Learning section.
+
+app = flask.Flask(__name__)
+@app.route('/')
+def cv_page():
+	return flask.render_template('CV.html')
+
+app.run()
