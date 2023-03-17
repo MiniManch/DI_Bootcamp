1094c75ef50d1c3df9dc8ab42e4669377899bf04
MiniManch <Emaneulrokah96@gmail.com>
2023-03-17 19:38:41 +0200

    adding some more logic to the turn check, character select, and almost finished the game'

--- Week-9/Day-3/Game-Characters/main.py
+++ Week-9/Day-3/Game-Characters/main.py
@@ -1,22 +1,46 @@
+import random
+
 class Character:
+  class_name = None
   def __init__(self,name):
-    self.name   = name
-    self.health = 20
-    self.attack = 10
+    self.name       = name
+    self.health     = 20
+    self.attack     = 10
 
   def basic_attack(self,other_char):
     other_char.health -= self.attack
 
+  def __gt__(self,other):
+    this_char = type(self).class_name
+    other_char = type(other).class_name
+
+    if this_char  == 'Mage' and other_char != 'Mage':
+      return True
+    if this_char == 'Druid' and other_char == 'Warrior':
+      return True
+    return False
+
+  def info(self):
+    return {'Health':self.health,'Attack':self.attack}
+
+  # @classmethod
+  # def __str__(cls):
+  #   return cls.class_name
 
 class Druid(Character):
-  def __init__(self):
+  class_name = 'Druid'
+  move_list  = ['help','meditate','animal_help','drain','attack']
+  move_on_enm = 'drain'
+
+  def __init__(self,name):
+    super().__init__(name)
     print('Im a druid, get ready to die !')
 
   def meditate(self):
     self.health += 10
-    print(f'{self.name} meditated 🧎‍♂️ hes calm now, and his health has increased by 10 ✨')
+    print(f'{self.name} meditated 🧎‍ hes calm now, and his health has increased by 10 ✨')
 
-  def animal_help(self):
+  def animal(self):
     self.attack += 5
     print(f'{self.name} has used Animal Help 🐾 his attack has increased by 5')
 
@@ -26,11 +50,16 @@ class Druid(Character):
     print(f'{self.name} has deal {attack} amount of damage on {other_char.name} 💢')
 
   def help(self):
-    return f' {self.name} You can use drain(a), get an animal to help you(b), or meditate(c)'
+    return f' {self.name} You can use -attack-, -drain-, get an -animal- to help you, or -meditate-'
 
 
 class Warrior(Character):
-  def __init__(self):
+  class_name = 'Warrior'
+  move_list  = ['help','brawl','train','intimidate','attack']
+  move_on_enm = ['brawl','intimidate']
+
+  def __init__(self,name):
+    super().__init__(name)
     print('Im a warrior 👨‍🎤 this will be easy!')
 
   def brawl(self,other_char):
@@ -42,19 +71,23 @@ class Warrior(Character):
   def train(self):
     self.health += 2
     self.attack += 2
-    print(f'{self.name} used train. 🏋️‍♂️ his attack and health are now increased by 2!')
+    print(f'{self.name} used train. 🏋️‍ his attack and health are now increased by 2!')
 
   def intimidate(self,other_char):
     other_char.attack -= 3
     print(f'{self.name} just flashed his muscles 🦾 theyre so big, that {other_char.name} damage has decreased by 3')
 
   def help(self):
-    return f' {self.name} you can try and intimidate(a) the enemy,train(b) to increase your strength, try get in a brawl(c)'
+    return f' {self.name} you can try and -intimidate- the enemy,-train- to increase your strength, simply -attack-, or try get in a -brawl-'
 
 
 class Mage(Character):
-  def __init__(self):
-    print('being a mage 🧙‍♂️ I can forsee exactly how this battle will pan out!')
+  class_name = 'Mage'
+  move_list  = ['help','cast','summon','curse','attack']
+  move_on_enm = ['curse','cast']
+  def __init__(self,name):
+    super().__init__(name)
+    print('being a mage 🧙‍ I can forsee exactly how this battle will pan out!')
     
   def curse(self,other_char):
     other_char.attack -= 2
@@ -64,29 +97,99 @@ class Mage(Character):
     self.attack += 3
     print(f'{self.name} has summoned a monster 👹 \n he can attack exactly +3 harder now')
 
-  def castspell(self,other_char):
+  def cast(self,other_char):
     attack = self.health/self.attack
     other_char.health -= attack
     print(f'{self.name} just cast a spell 🌪 \n {other_char.name}\'s health just decreased by {attack}')
 
   def help(self):
-    return f' {self.name} You can Cast a spell(a), summon a monster(b), or curse(c) the enemy!'
-
+    return f' {self.name} You can -Cast- a spell, -summon- a monster,attack or -curse- the enemy!'
 
 def choose_char():
-  char_list = ['Mage','Warrior','Druid']
+  char_list = [Mage,Warrior,Druid]
   player_list = ['player_1','player_2']
 
   for index,player in enumerate(player_list):
     player_name = input('Hey there, please choose a name \n')
+    while type(player_list[0])!= str:
+      while player_name == player_list[0]['Name']:
+        player_name = input('Hey there, please choose a name \n')
+      break
     player_char = input(f'Hey {player_name}, please choose a Character! (Mage,Warrior or Druid) \n').lower().capitalize()
-    while player_char not in char_list:
-      player_char = input(f'Hey {player_name}, please choose a Character! (Mage,Warrior or Druid) \n').lower().capitalize()
-    player_list[index] = {player:[player_name,player_char]}
+
+    i = True
+    counter = 0
+    while i == True:
+      for char_type in char_list:
+        if counter == len(char_list):
+          player_char = input(f'Hey {player_name}, please choose a Character! (Mage,Warrior or Druid) \n').lower().capitalize()
+          counter = 0
+        if player_char == char_type.class_name:
+          player_char = char_type(player_name)
+          i = False
+        counter +=1
+
+    player = {'Name':player_name,'Char':player_char}
+    player_list[index] = player
   return player_list
+
+
+# change turns decide who starts, perhaps make it logical based on char type
+def first_turn(player_list):
+  this_char  = player_list[0]['Char']
+  other_char = player_list[1]['Char']
+
+  if this_char > other_char and other_char > this_char:
+    return 0
+  return random.randint(0,1)  
+
+# if health is 0 winner is declared
+def play(turn,player_list,attack,health,name,char):  
+  print(f'{name} its your turn! \n you have Attack of:{attack} and Health of {health}')
+  choice = input(f"{name}\n {char.help()} \n").lower()
+  while choice not in char.move_list:
+    choice = input(f"{name}\n {char.help()} \n").lower()
+  return choice
+
+def check_win(player_list):
+  for player in player_list:
+    if player['Char'].health == 0:
+      return True
+  return False
+
 
 def game():
   player_list = choose_char()
+  turn = first_turn(player_list)
+  while not check_win(player_list):
+    if turn == 0:
+      other_char = 1
+    elif turn ==1:
+      other_char = 0
 
-game()
+    info = player_list[turn]['Char'].info()
+    attack = info['Attack']
+    health = info['Health']
+    name   = player_list[turn]['Name']
+    char   = player_list[turn]['Char']
+    other_char = player_list[other_char]['Char']
+
+    print('other',char)
+    choice = play(turn,player_list,attack,health,name,char)
+    if choice == 'attack':
+      eval(f'{char}.basic_attack({other_char})')
+    if choice in type(char).move_on_enm:
+      eval(f'{char}.{choice}({other_char})')
+    else:
+      exec(f'{char}.{choice}()')
+
+    if turn == 0:
+      turn = 1
+    elif turn == 1:
+      turn = 0
+
+
+  return choice
+
+print(game())
   
