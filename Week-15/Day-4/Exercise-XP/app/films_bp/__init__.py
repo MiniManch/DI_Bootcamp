da7cff975734936d984f731c6d63e86c1ac0b620
Emmanuel Rokah <36491225+MiniManch@users.noreply.github.com>
2023-04-22 21:18:01 +0300

    Delete Week-15/Day-4/Exercise-XP directory

--- Week-15/Day-4/Exercise-XP/app/films_bp/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-films_bp = Blueprint('films_bp', __name__, template_folder='views', static_folder='static')
-
-
-from app.films_bp import routes
-from app.films_bp import models
